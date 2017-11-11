b93bf75bc4a86adc1fd1d376936f793e9a58c665
Yago <jagogc@gmail.com>
2017-11-11 18:59:24 +0000

    PropTypes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Movie from './Movie'
 const movies = [
   {
     id: 1,
-    title: 'Star Wars'
+    title: 'Star Wars',
+    desc: 'A space movie'
   },
   {
     id: 2,
@@ -25,13 +26,31 @@ const movies = [
 
 class App extends Component {
 
+  state = {
+    movies: []
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=c857fa67fba523ad3ce66df18e7ab279&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      console.log(movies);
+      this.setState({
+        movies: movies.results
+      })
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie} /> )}
+        {movies.map(movie => <Movie key={movie.id} movie={movie} desc={movie.desc} /> )}
       </div>
     );
   }
